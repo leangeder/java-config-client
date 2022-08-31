12cf9ca7a2dad863e20bd80198aaf101ce66281f
Gregory BULGARE <gregory.bulgare@underwriteme.co.uk>
2022-08-31 11:27:19 +0100

    test

---
+++ src/test/java/uk/co/underwriteme/ruleengine/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.underwriteme.ruleengine.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
