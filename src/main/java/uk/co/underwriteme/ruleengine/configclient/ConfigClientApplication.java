7ca906ad57b9c37702e663f4254e5cf391b0e849
Gregory BULGARE <gregory.bulgare@underwriteme.co.uk>
2022-08-31 19:26:47 +0100

    test

--- src/main/java/uk/co/underwriteme/ruleengine/configclient/ConfigClientApplication.java
+++ src/main/java/uk/co/underwriteme/ruleengine/configclient/ConfigClientApplication.java
@@ -1,55 +1,12 @@
 package uk.co.underwriteme.ruleengine.configclient;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-@RefreshScope
 public class ConfigClientApplication {
-
-	@Value("${describe: Test Message}")
-	private String message;
-
-	@GetMapping("/message")
-	public String getPlans() {
-		return message;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args);
 	}
 }
-
-
-
-
-
-
-
-
-
-
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.cloud.context.config.annotation.RefreshScope;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
-// 
-// @RefreshScope
-// @RequestMapping("/rest")
-// @RestController
-// public class MessageResource {
-// 
-//     @Value("${message: Default Hello}")
-//     private String message;
-// 
-//     @GetMapping("/message")
-//     public String message() {
-//         return message;
-//     }
-// }
